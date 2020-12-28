26c75c62f280b6470727720c75a19fadf7db7228
@lvim <andersonalvimdesouza@MacBook-Pro-de-lvim.local>
2020-12-28 12:13:40 -0200

    primeiro commit

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import *
+import scripts
+import pyrebase, requests
+
+config = {
+    "apiKey": "AIzaSyDRsKltjDQHVUrspqP_NbhZdc4xk2PszQA",
+    "authDomain": "kaffaproject.firebaseapp.com",
+    "databaseURL": "https://kaffaproject-default-rtdb.firebaseio.com",
+    "projectId": "kaffaproject",
+    "storageBucket": "kaffaproject.appspot.com",
+    "messagingSenderId": "309346705957",
+    "appId": "1:309346705957:web:6194b3ffced5d72e3915cc",
+    "measurementId": "G-4GJ1WD2VWJ"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    return render_template('index.html')
+
+@app.route('/task1.html', methods=['GET', 'POST'])
+def basic1():
+    if request.method == 'POST':
+        if request.form.get('submit', False) == 'submit':
+            cnpj = request.form['name']
+            result = scripts.validaCnpj(cnpj)
+            return render_template('task1.html', x=result)
+    return render_template('task1.html')
+
+@app.route('/task2.html', methods=['GET', 'POST'])
+def basic2():
+    if request.method == 'POST':
+        if request.form.get('submit', False) == 'submit':
+            cnpj = request.form['name']
+            result = scripts.validaCnpj1(cnpj)
+            return render_template('task2.html', x=result)
+    return render_template('task2.html')
+
+@app.route('/task3.html', methods=['GET','POST'])
+def basic3():
+    if request.method == 'POST':
+        if request.form.get('submit', False) == 'submit':
+            x1 = request.form['coordx1']
+            y1 = request.form['coordy1']
+            width1 = request.form['width1']
+            height1 = request.form['height1']
+            x2 = request.form['coordx2']
+            y2 = request.form['coordy2']
+            width2 = request.form['width2']
+            height2 = request.form['height2']
+
+            result = scripts.checkAndComputeIntersectionTwoRectangles(x1,y1,width1,height1,x2,y2,width2,height2)
+
+            if result[0] == False:
+                return render_template('task3.html', x='There is no intersection between this rectangles')
+            else:
+                return render_template('task3.html', x='There is intersection between this rectangles')
+    return render_template('task3.html')
+
+@app.route('/task4.html', methods=['GET','POST'])
+def basic4():
+    if request.method == 'POST':
+        if request.form.get('submit', False) == 'submit':
+            x1 = request.form['coordx1']
+            y1 = request.form['coordy1']
+            width1 = request.form['width1']
+            height1 = request.form['height1']
+            x2 = request.form['coordx2']
+            y2 = request.form['coordy2']
+            width2 = request.form['width2']
+            height2 = request.form['height2']
+
+            result = scripts.checkAndComputeIntersectionTwoRectangles(x1,y1,width1,height1,x2,y2,width2,height2)
+
+            if result[0] == False:
+                return render_template('task4.html', x='There is no intersection between this rectangles')
+            else:
+                return render_template('task4.html', x=result[1])
+    return render_template('task4.html')
+
+@app.route("/task5.html", methods=["GET","POST"])
+def basic5():
+    if request.method == 'POST':
+        if request.form.get('submit', False) == 'add':
+            name = request.form['name']
+            db.child("todo").push(name)
+            todo = db.child("todo").get()
+            to = todo.val()
+            return render_template('task5.html', t=to.values())
+        elif request.form.get('submit', False) == 'delete':
+            db.child("todo").remove()
+        return render_template('task5.html')
+    return render_template("task5.html")
+
+@app.route("/task6.html", methods=["GET","POST"])
+def basic6():
+    request = requests.get('http://worldclockapi.com/api/json/utc/now')
+    address_data = request.json()
+    currentTime = address_data['currentDateTime']
+    dayOfTheWeek = address_data['dayOfTheWeek']
+
+    if currentTime:
+        return render_template("task6.html", x=currentTime, y=dayOfTheWeek)
+    return render_template("task6.html")
+
+@app.route("/task7.html", methods=["GET","POST"])
+def basic7():
+    request = requests.get('http://worldclockapi.com/api/json/utc/now')
+    address_data = request.json()
+    if address_data:
+        return render_template('task7.html', x=address_data)
+    return render_template("task7.html")
+
+@app.route("/task8.html", methods=["GET","POST"])
+def basic8():
+    return render_template("task8.html")
+
+@app.route("/task9.html", methods=["GET","POST"])
+def basic9():
+    return render_template("task9.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scripts.py
@@ -0,0 +1,63 @@
+import re
+
+def validaCnpj(cnpj):
+    expr = re.compile('\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}')
+    if expr.search(cnpj):
+        return 'It is a valide CNPJ'
+    else:
+        return 'It is not a valide CNPJ'
+
+def validaCnpj1(cnpj):
+    aux = cnpj.split('-')
+    digitoVerificador = aux[1]
+    aux1 = aux[0]
+    aux = aux1[:2] + aux1[3:6] + aux1[7:10] + aux1[11:16]
+    aux1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    soma = 0
+    digito1 = 0
+    digito2 = 0
+    for elemento in range(0, 12, 1):
+        soma += int(aux[elemento]) * aux1[elemento]
+    resto = soma % 11
+    if resto < 2:
+        digito1 = 0
+    else:
+        digito1 = 11 - resto
+    aux = aux + str(digito1)
+    aux1 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    soma = 0
+    for elemento in range(0, 13, 1):
+        soma += int(aux[elemento]) * aux1[elemento]
+    resto = 0
+    resto = soma % 11
+    if resto < 2:
+        digito2 = 0
+    else:
+        digito2 = 11 - resto
+    dv = str(digito1) + str(digito2)
+    if digitoVerificador == dv:
+        return 'CNPJ is valid'
+    else:
+        return 'CNPJ is not valid'
+
+def checkAndComputeIntersectionTwoRectangles(x1,y1,width1,height1,x2,y2,width2,height2):
+    K = int(x1)
+    L = int(y1)
+    M = int(width1) + 1
+    N = int(height1) + 1
+
+    P = int(x2)
+    Q = int(y2)
+    R = int(width2) + 1
+    S = int(height2) + 1
+
+    left = max(K, P)
+    right = min(M, R)
+    bottom = max(L, Q)
+    top = min(N, S)
+
+    if left < right and bottom < top:
+        intersection = (right - left) * (top - bottom)
+        return [True, intersection]
+    else:
+        return [False, 0]
